2a1caa0735803b3e56d17e5267732868d4ece39d
Joseph Chipperfield <joechip1709@gmail.com>
2021-07-22 17:06:03 +0100

    getting error message for event listener

--- Test_Project/app.js
+++ Test_Project/app.js
@@ -1,6 +1,13 @@
-var URL = ('https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=');
-var key = ('4a03681667fdda3a1b49ffbf805f1f19');
+var baseURL = ('https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=');
+var key = ('&apiKey=4a03681667fdda3a1b49ffbf805f1f19');
+var searchEntry = document.getElementById('#name');
+const submitButton = document.getElementById('#submitButton');
 
+function searchCharacter() {
+    var URL = baseURL + searchEntry + key;
+    console.log('hello');
+};
 
-
-console.log('hello');
+submitButton.addEventListener('click', () => {
+    searchCharacter();
+});
