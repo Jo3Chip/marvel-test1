b793e7d521300a46ac0dced223d32f5b4a219a7d
Joseph Chipperfield <joechip1709@gmail.com>
2021-07-23 13:09:15 +0100

    building in progress

--- Test_Project/app.js
+++ Test_Project/app.js
@@ -1,11 +1,11 @@
 var baseURL = ('https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=');
 var key = ('&apiKey=4a03681667fdda3a1b49ffbf805f1f19');
-var searchEntry = document.getElementById('#name');
-const submitButton = document.getElementById('#submitButton');
+var searchEntry = document.getElementById('name');
+const submitButton = document.getElementById('submitButton');
 
 function searchCharacter() {
-    var URL = baseURL + searchEntry + key;
-    console.log('hello');
+    var URL = (baseURL + searchEntry.value + key);
+    console.log(URL);
 };
 
 submitButton.addEventListener('click', () => {
